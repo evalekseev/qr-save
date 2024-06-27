b80e3c35b1d87c3bbd8cebe8b6dd77337cf6814a
Evgeniy <alexeevev@gmail.com>
2024-06-27 09:33:55 +0300

    chg

--- src/downloads.tsx
+++ src/downloads.tsx
@@ -1,49 +1,50 @@
 import { QRCode, QRSvg } from 'sexy-qr'
 import fileSaver from 'file-saver'
 import styles from './styles.module.css'
+import { useMemo } from 'react'
 // import reactLogo from './assets/react.svg'
 export const Downloads = () => {
-  //   const svgCodeMportal = useMemo(() => {
-  //     const qrCode = new QRCode({
-  //       content: 'MLyaaaaaaa',
-  //       ecl: 'M',
-  //     })
-
-  //     const { svg } = new QRSvg(qrCode, {
-  //       fill: '#34324E',
-  //       cornerBlocksAsCircles: false,
-  //       size: 200,
-  //       radiusFactor: 0.75,
-  //       cornerBlockRadiusFactor: 2,
-  //       roundOuterCorners: true,
-  //       roundInnerCorners: true,
-  //     })
-
-  //     return svg
-  //   }, [])
-
-  const svgCodeBase = (() => {
+  const svgCodeMportal = useMemo(() => {
     const qrCode = new QRCode({
-      content: 'MLyaaaaaaa',
-      ecl: 'M', // 'L' | 'M' | 'Q' | 'H'
+      content: 'MLyaaaaaaa22',
+      ecl: 'M',
     })
 
-    const qrSvg = new QRSvg(qrCode, {
-      fill: '#182026',
-      cornerBlocksAsCircles: true,
-      size: 200, // px
-      radiusFactor: 0.75, // 0-1
-      cornerBlockRadiusFactor: 2, // 0-3
+    const { svg } = new QRSvg(qrCode, {
+      fill: '#34324E',
+      cornerBlocksAsCircles: false,
+      size: 200,
+      radiusFactor: 0.75,
+      cornerBlockRadiusFactor: 2,
       roundOuterCorners: true,
       roundInnerCorners: true,
-      preContent: '<!-- QR Code -->',
     })
 
-    return qrSvg.svg
-  })()
+    return svg
+  }, [])
 
-  //   const svgCode = svgCodeMportal
-  const svgCode = svgCodeBase
+  // const svgCodeBase = (() => {
+  //   const qrCode = new QRCode({
+  //     content: 'MLyaaaaaaa',
+  //     ecl: 'M', // 'L' | 'M' | 'Q' | 'H'
+  //   })
+
+  //   const qrSvg = new QRSvg(qrCode, {
+  //     fill: '#182026',
+  //     cornerBlocksAsCircles: true,
+  //     size: 200, // px
+  //     radiusFactor: 0.75, // 0-1
+  //     cornerBlockRadiusFactor: 2, // 0-3
+  //     roundOuterCorners: true,
+  //     roundInnerCorners: true,
+  //     preContent: '<!-- QR Code -->',
+  //   })
+
+  //   return qrSvg.svg
+  // })()
+
+  const svgCode = svgCodeMportal
+  // const svgCode = svgCodeBase
 
   const markup = { __html: svgCode }
 
