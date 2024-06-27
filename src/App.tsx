527970bc0e3d6114ae18679ab9b5e24030eaae8a
Evgeniy <alexeevev@gmail.com>
2024-06-27 09:04:20 +0300

    add app

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import { Downloads } from './downloads'
+
+function App() {
+  return <Downloads />
+}
+
+export default App
